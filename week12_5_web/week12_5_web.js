cd583d185799a3828d65846dd5a78d76548597da
vita hsu <91251449+vitahsu030812@users.noreply.github.com>
2022-05-20 09:36:32 +0800

    Update week12_5_web.js

--- week12_5_web/week12_5_web.js
+++ week12_5_web/week12_5_web.js
@@ -16,13 +16,19 @@ function draw() {
         rect(50, 100, 150, 2);
     if (choice == 2)
         rect(50, 150, 150, 2);
-    text("Choice 1", 50, 50);
-    text("Choice 2", 50, 100);
-    text("Choice 3", 50, 150);
+    if (choice == 3)
+        rect(50, 200, 150, 2);
+    if (choice == 4)
+        rect(50, 250, 150, 2);
+    text("中午吃泡麵", 50, 50);
+    text("中午吃UberEat", 50, 100);
+    text("中午吃FoodPanda", 50, 150);
+    text("中午吃牛排", 50, 200);
+    text("中午吃大餐", 50, 250);
 }
 
 function mousePressed() {
-    choice = int(random(3));
+    choice = int(random(5));
 }
 
 function initializeFields() {
